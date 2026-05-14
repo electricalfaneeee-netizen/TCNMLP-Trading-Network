b8fc0d6c27efd483a3e67b243fe0c5208095c8e1
coffeecrisp <electricalfaneeee@gmail.com>
2026-05-14 19:52:27 -0400

    changed encoder architecture to actually bottleneck features

--- network.py
+++ network.py
@@ -8,22 +8,21 @@ from pathlib import Path
 
 device = torch.device("cuda" if torch.cuda.is_available() else "xpu" if hasattr(torch, "xpu") and torch.xpu.is_available() else "cpu")
 
-script_dir = Path(__file__).parent
-
-class TCNEncoder(nn.Module):
+class TCNEmbedder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.cnn1 = nn.Conv1d(5, 10, kernel_size=3, padding=1)
-        self.cnn2 = nn.Conv1d(5, 10, kernel_size=7, padding=3)
-        self.cnn3 = nn.Conv1d(5, 10, kernel_size=15, padding=7)
-        self.batchNorm = nn.BatchNorm1d(30)
+        self.cnn1 = nn.Conv1d(5, 16, kernel_size=3, stride=2, padding=1)
+        self.cnn2 = nn.Conv1d(16, 32, kernel_size=3, stride=2, padding=1)
+        self.cnn3 = nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=2, dilation=2)
+        self.batchNorm = nn.BatchNorm1d(64)
         self.activation = nn.Tanh()
 
     def forward(self, x):
-        x1, x2, x3 = self.cnn1(x), self.cnn2(x), self.cnn3(x)
-        out = torch.cat([x1, x2, x3], dim=1)
+        x = self.cnn1(x)
+        x = self.cnn2(x)
+        x = self.cnn3(x)
 
-        return self.activation(self.batchNorm(out)).transpose(1, 2)
+        return self.activation(self.batchNorm(x)).transpose(1, 2)
 
 class TCNMLP(nn.Module):
     def __init__(self, encoder):
